5986c1eb29c947d031957e9bd41e67cae3b80ef8
Paulo Reis <paauloreis@yahoo.com.br>
2022-01-07 05:00:57 -0300

    pikachu terminado

--- pikachu/p3.js
+++ pikachu/p3.js
@@ -5,16 +5,18 @@ var mensagem;
 
 function pikachu1(){
     while(mensagem != 1 || mensagem != 2){
-        mensagem = prompt('O que fazer agora? \n \n 1 - Enfrentar o Gyarados \n \n 2 - Checar com mais calma');
+        mensagem = prompt('O que fazer agora? \n \n 1 - Ir sozinho mesmo, eu me garanto. \n \n 2 - Chamar o treinador');
 
-        if (mensagem == 1){
-            window.alert('É isso, vamos pra cima dele!!');
-            return window.location.href = './pikachu3.html';
+        if (mensagem == 2){
+            window.alert('Chamar o treinador??');
+            window.alert('Mas você tem pouco tempo...');
+            window.alert('Tudo bem, vamos la né...');
+            return window.location.href = './pikachu4.html';
         }
-        else if (mensagem == 2){
+        else if (mensagem == 1){
             document.getElementsByClassName('conteudo')[0].innerHTML=`<h1>Você Perdeu</h1>
             <img class="gifLose" src="../images/pikachu-lose.gif" alt="">
-            <p class= "descricao">Você não agiu à tempo, o que fez com que o gyarados conseguisse destruir todo o sistema, o que gerou um baita problema para o Mundo Nintendo</p>
+            <p class= "descricao">Da onde você tirou que poderia enfrentar um Gyarados e um Rhydon sozinhx?? Você entrou na porrada, ficou feio pra você...</p>
             <p> Tenta novamente ai, vai ficar cada vez mais facil</p>
             <a href="../index.html"><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a>
         </main>`;
